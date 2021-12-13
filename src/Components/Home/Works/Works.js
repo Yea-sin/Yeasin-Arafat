90697eacfa0322dd3a750d3549f65cffc4856f75
Yea-sin <yeasinn2002@gmail.com>
2021-12-13 18:09:08 +0600

    modified

--- src/Components/Home/Works/Works.js
+++ src/Components/Home/Works/Works.js
@@ -7,202 +7,209 @@ import { AiFillGithub } from "react-icons/ai";
 
 const Works = () => {
   return (
-    <Container className="space-container" id="projects">
-      <div
-        className="readex-font mb-5"
-        data-aos="fade-down"
-        data-aos-easing="linear"
-        data-aos-duration="900">
-        <h2>Works_</h2>
-        <small>Projects I Made</small>
-      </div>
-      <Row lg={2} md={2} sm={1} xs={1} className="align-items-center g-5">
-        <Col>
-          <div className="project-img">
-            <img
-              data-aos="zoom-in-up"
-              data-aos-easing="linear"
-              data-aos-duration="1000"
-              src="https://i.ibb.co/Ph0HrFh/3-devices-black.png"
-              width={"100%"}
-              alt=""
-            />
-          </div>
-        </Col>
-        <Col
-          className="text-start"
-          data-aos="fade-up"
+    <div className="space-container">
+      <Container id="projects">
+        <div
+          className="readex-font mb-5"
+          data-aos="fade-down"
           data-aos-easing="linear"
-          data-aos-duration="1000">
-          <div className="francois mb-3">
-            <h2 className="fw-bold">Drive-home - Car Niche Website</h2>
-            <small className="text-secondary">Web App</small>
-          </div>
-          <div className="readex-font">
-            <p>
-              <BsChevronCompactRight /> A responsive, fully-functional, admin
-              secure niche website using MERN stack
-            </p>
-            <p>
-              <BsChevronCompactRight /> Allowing customers to create an account,
-              browse products, order products & write reviews.
-            </p>
-            <p>
-              <BsChevronCompactRight /> Admins have the ability to add new
-              products, remove existing products, remove user products and
-              create new admin accounts.
-            </p>
-            <p>
-              <BsChevronCompactRight /> Reviews are dynamic
-            </p>
-          </div>
-          <div className="row text-light gy-2">
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #React
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Javascript
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #MUI
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Node.js
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #express.js
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #firebase
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Heroku
-            </small>
-          </div>
-          <div className="mt-3">
-            <small className="links">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://drive-home-884b0.web.app/">
-                <RiEarthLine className="me-1" />
-                Live Site
-              </a>
-            </small>
-            <small className="links mx-3">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://github.com/Yea-sin/drive-home-client-side">
-                <AiFillGithub className="me-1" />
-                Client Side
-              </a>
-            </small>
-            <small className="links">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://github.com/Yea-sin/drive-home-server-side">
-                <AiFillGithub className="me-1" />
-                Server Side
-              </a>
-            </small>
-          </div>
-        </Col>
-      </Row>
-      <Row lg={2} md={2} sm={1} xs={1} className="align-items-center mt-5 g-5">
-        <Col>
-          <div
-            className="project-img"
-            data-aos="zoom-in-ip"
+          data-aos-duration="900">
+          <h2>Works_</h2>
+          <small>Projects I Made</small>
+        </div>
+        <Row lg={2} md={2} sm={1} xs={1} className="align-items-center g-5">
+          <Col>
+            <div className="project-img">
+              <img
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                src="https://i.ibb.co/Ph0HrFh/3-devices-black.png"
+                width={"100%"}
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col
+            className="text-start"
+            data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1000">
-            <img
-              data-aos="zoom-in-up"
+            <div className="francois mb-3">
+              <h2 className="fw-bold">Drive-home - Car Niche Website</h2>
+              <small className="text-secondary">Web App</small>
+            </div>
+            <div className="readex-font">
+              <p>
+                <BsChevronCompactRight /> A responsive, fully-functional, admin
+                secure niche website using MERN stack
+              </p>
+              <p>
+                <BsChevronCompactRight /> Allowing customers to create an
+                account, browse products, order products & write reviews.
+              </p>
+              <p>
+                <BsChevronCompactRight /> Admins have the ability to add new
+                products, remove existing products, remove user products and
+                create new admin accounts.
+              </p>
+              <p>
+                <BsChevronCompactRight /> Reviews are dynamic
+              </p>
+            </div>
+            <div className="row text-light gy-2">
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #React
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Javascript
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #MUI
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Node.js
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #express.js
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #firebase
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Heroku
+              </small>
+            </div>
+            <div className="mt-3">
+              <small className="links">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://drive-home-884b0.web.app/">
+                  <RiEarthLine className="me-1" />
+                  Live Site
+                </a>
+              </small>
+              <small className="links mx-3">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/Yea-sin/drive-home-client-side">
+                  <AiFillGithub className="me-1" />
+                  Client Side
+                </a>
+              </small>
+              <small className="links">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/Yea-sin/drive-home-server-side">
+                  <AiFillGithub className="me-1" />
+                  Server Side
+                </a>
+              </small>
+            </div>
+          </Col>
+        </Row>
+        <Row
+          lg={2}
+          md={2}
+          sm={1}
+          xs={1}
+          className="align-items-center mt-5 g-5">
+          <Col>
+            <div
+              className="project-img"
+              data-aos="zoom-in-ip"
               data-aos-easing="linear"
-              data-aos-duration="1000"
-              src="https://i.ibb.co/7Kjpd3m/3-devices-black.png"
-              width={"100%"}
-              alt=""
-            />
-          </div>
-        </Col>
-        <Col
-          className="text-start"
-          data-aos="fade-up"
-          data-aos-easing="linear"
-          data-aos-duration="1000">
-          <div className="francois mb-3">
-            <h2 className="fw-bold">Get-Food - Food Delivery Website</h2>
-            <small className="text-secondary">Web App</small>
-          </div>
-          <div className="readex-font">
-            <p>
-              <BsChevronCompactRight /> Functional Food delivery, fully
-              responsive website using MERN stack.
-            </p>
-            <p>
-              <BsChevronCompactRight /> Users can order/buy Food online through
-              this website
-            </p>
-            <p>
-              <BsChevronCompactRight /> Users have the ability to add new
-              products, remove existing products
-            </p>
-          </div>
-          <div className="row text-light gy-2">
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #React
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Javascript
-            </small>
+              data-aos-duration="1000">
+              <img
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                src="https://i.ibb.co/7Kjpd3m/3-devices-black.png"
+                width={"100%"}
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col
+            className="text-start"
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1000">
+            <div className="francois mb-3">
+              <h2 className="fw-bold">Get-Food - Food Delivery Website</h2>
+              <small className="text-secondary">Web App</small>
+            </div>
+            <div className="readex-font">
+              <p>
+                <BsChevronCompactRight /> Functional Food delivery, fully
+                responsive website using MERN stack.
+              </p>
+              <p>
+                <BsChevronCompactRight /> Users can order/buy Food online
+                through this website
+              </p>
+              <p>
+                <BsChevronCompactRight /> Users have the ability to add new
+                products, remove existing products
+              </p>
+            </div>
+            <div className="row text-light gy-2">
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #React
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Javascript
+              </small>
 
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Node.js
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #express.js
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #firebase
-            </small>
-            <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
-              #Heroku
-            </small>
-          </div>
-          <div className="mt-3">
-            <small className="links">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://drive-home-884b0.web.app/">
-                <RiEarthLine className="me-1" />
-                Live Site
-              </a>
-            </small>
-            <small className="links mx-3">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://github.com/Yea-sin/drive-home-client-side">
-                <AiFillGithub className="me-1" />
-                Client Side
-              </a>
-            </small>
-            <small className="links">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://github.com/Yea-sin/drive-home-server-side">
-                <AiFillGithub className="me-1" />
-                Server Side
-              </a>
-            </small>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Node.js
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #express.js
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #firebase
+              </small>
+              <small className="col-md-3 ms-2 p-1 rounded bg-secondary">
+                #Heroku
+              </small>
+            </div>
+            <div className="mt-3">
+              <small className="links">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://drive-home-884b0.web.app/">
+                  <RiEarthLine className="me-1" />
+                  Live Site
+                </a>
+              </small>
+              <small className="links mx-3">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/Yea-sin/drive-home-client-side">
+                  <AiFillGithub className="me-1" />
+                  Client Side
+                </a>
+              </small>
+              <small className="links">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/Yea-sin/drive-home-server-side">
+                  <AiFillGithub className="me-1" />
+                  Server Side
+                </a>
+              </small>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
